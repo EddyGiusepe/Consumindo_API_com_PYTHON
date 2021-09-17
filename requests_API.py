85b767f0363c92acf06153e5a69df75d4549de61
EddyGiusepe <eddychirinos.unac@gmail.com>
2021-09-17 16:07:38 -0300

    script final para pegar os Dados da página ViaCEP.

--- requests_API.py
+++ requests_API.py
@@ -1,7 +1,30 @@
+# Data Scientist Jr.: Dr.Eddy Giusepe Chirinos Isidro
+
+# Links de estudos:
+# https://www.youtube.com/watch?v=vrIQM8257Zc
+# https://www.youtube.com/watch?v=IdviYZIQLlA
 import requests
 import os
 
 # A seguir: o comando limpa o terminal depois de rodar o Script
-os.system('clear')
+# os.system('clear')
 
+print("##############################################")
+print("## Dados respeito ao CEP solicitado ##")
+print("##############################################")
+print("")
+cep = input("Digite o CEP (número de apenas 8 dígitos):")
+r = requests.get("https://viacep.com.br/ws/{}/json/".format(cep))
+
+data = r.json()
+print("Aqui imprimimos o .json total: ")
+print(data)
+
+print("")
+print("CEP: {}".format(data["cep"]))
+print("Logradouro: {}".format(data["logradouro"]))
+print("Bairro: {}".format(data["bairro"]))
+print("Localidade: {}".format(data["localidade"]))
+print("UF: {}".format(data["uf"]))
+print("")
 
