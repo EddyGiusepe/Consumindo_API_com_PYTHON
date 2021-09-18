ae825127d2a101a7a0b2e909ee64595133e92524
EddyGiusepe <eddychirinos.unac@gmail.com>
2021-09-18 01:06:24 -0300

    usando requests: GET

---
+++ Get_Post_Patch_Delete.py
@@ -0,0 +1,55 @@
+# Data Scientist Jr.: Dr.Eddy Giusepe Chirinos Isidro 
+# Exemplo baseado no link: https://www.youtube.com/watch?v=IdviYZIQLlA
+# Meu banco de Dados (gmail Eddy): https://console.firebase.google.com/project/teste-6bb33/database/teste-6bb33-default-rtdb/data
+
+print("######################################")
+print("## Biblioteca requets (REQUISIÇÕES)  ##")
+print("######################################")
+print("")
+# Temos as seguintes requisições:
+# GET --> Pegar informações do banco de Dados (para nosso caso)
+# POST --> Criar uma nova informação (inserir uma nova informaçao)
+# PATCH --> Editar uma informação (atualizar uma informação)
+# DELETE --> Deletar informações
+
+
+
+print("####################")
+print("## Exemplo_1: GET ##")
+print("####################")
+print("")
+# Importamos as nossa Biblioteca
+import requests
+print("O site na qual fazemos um GET é: https://docs.awesomeapi.com.br/api-de-moedas")
+requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+
+print("A requisição será um número (entre 200-299) que significa que DEU CERTO: {}".format(requisicao))
+print("")
+print("Agora vamos ver essa informação no formato .json (no Python seria um dicionário): ")
+print(requisicao.json())
+print("")
+
+
+print("####################")
+print("## Exemplo_2: GET ##")
+print("####################")
+print("")
+# Neste exemplo utilizaremos o site: firebase (banco de Dados da Google)
+print("O site na qual fazemos um GET é: https://firebase.google.com/?gclid=CjwKCAjw-ZCKBhBkEiwAM4qfFyeyBBgjFYhdCtNYCNJDutYtDh6cUQWdrIFkMguRH6F-9Hv9RxmtsxoC04YQAvD_BwE&gclsrc=aw.ds")
+requisicao = requests.get("https://teste-6bb33-default-rtdb.firebaseio.com/.json") # No final adicionei .json porque a página fala isso
+
+print("A requisição será um número (entre 200-299) que significa que DEU CERTO: {}".format(requisicao))
+print("")
+print("Agora vamos ver essa informação no formato .json (no Python seria um dicionário): ")
+print(requisicao.json())
+print("")
+
+
+
+print("####################")
+print("## Exemplo_3: POST ##")
+print("####################")
+print("")
+# Neste exemplo utilizaremos o site: firebase (banco de Dados da Google)
+print("O site na qual fazemos um GET é: https://firebase.google.com/?gclid=CjwKCAjw-ZCKBhBkEiwAM4qfFyeyBBgjFYhdCtNYCNJDutYtDh6cUQWdrIFkMguRH6F-9Hv9RxmtsxoC04YQAvD_BwE&gclsrc=aw.ds")
+requisicao = requests.get("https://teste-6bb33-default-rtdb.firebaseio.com/.json") # No final adicionei .json porque a página fala isso
